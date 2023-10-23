41259705d4386c11138b706b52f922f64e8f36ef
Dominik Drexler <dominik.drexler@gmx.net>
2023-10-23 10:07:07 +0200

    Squashed 'src/external/mimir-iw/' content from commit bc98dff
    git-subtree-dir: src/external/mimir-iw
    git-subtree-split: bc98dff68a1a87a17bf7b9ed8a3d4f260209ec33

---
+++ src/public/iwr.cpp
@@ -0,0 +1,60 @@
+#include "utils.hpp"
+
+#include "../private/pddl/parsers.hpp"
+#include "../private/planners/iw_search.hpp"
+#include "../private/planners/goal_test.hpp"
+#include "../private/planners/dlplan_utils.hpp"
+
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+using namespace mimir;
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 6) {
+        throw std::runtime_error("Correct usage: ./iwr <domain:str> <problem:str> <arity:int> <sketch:str> <enable_grounding:bool>");
+    }
+    std::cout << "Started iwr search." << std::endl;
+    std::string domain_filename(argv[1]);
+    std::string problem_filename(argv[2]);
+    int arity = atoi(argv[3]);
+    std::string sketch_filename(argv[4]);
+    bool enable_grounding = utils::to_bool(argv[5]);
+    parsers::DomainParser domain_parser(domain_filename);
+    auto domain = domain_parser.parse();
+    parsers::ProblemParser problem_parser(problem_filename);
+    auto problem = problem_parser.parse(domain);
+
+    auto vocabulary = planners::construct_vocabulary(domain);
+    auto instance = planners::construct_instance(problem, vocabulary);
+    auto sketch = planners::read_sketch(vocabulary, sketch_filename);
+    auto goal_test = std::make_unique<planners::SketchGoalTest>(problem, instance, sketch);
+
+    auto successor_generator_type = planners::SuccessorGeneratorType::LIFTED;
+    if (enable_grounding) successor_generator_type = planners::SuccessorGeneratorType::GROUNDED;
+
+    planners::IWSearch planner(problem, successor_generator_type, std::move(goal_test), arity);
+
+    formalism::ActionList plan;
+    bool found_plan = planner.find_plan(plan);
+    std::cout << std::endl;
+    std::cout << "Overall search statistics:" << std::endl;
+    planner.print_statistics();
+
+    if (found_plan)
+    {
+        std::cout << "Plan length: " << plan.size() << std::endl;
+        std::ofstream plan_file("plan.txt");
+        for (const auto& action : plan)
+        {
+            plan_file << action << std::endl;
+        }
+        plan_file.close();
+    } else {
+        std::cout << "No solution found!" << std::endl;
+    }
+    return 0;
+}
---
+++ src/public/utils.cpp
@@ -0,0 +1,17 @@
+#include "utils.hpp"
+
+#include <algorithm>
+#include <sstream>
+
+
+namespace utils {
+
+bool to_bool(std::string str) {
+    std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+    std::istringstream is(str);
+    bool b;
+    is >> std::boolalpha >> b;
+    return b;
+}
+
+}
